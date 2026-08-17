cf54869cd1f68084b43f4748b5b148feaa3d9baa
eman-atia <eman.atiya@ecs-co.com>
2026-08-17 15:27:41 +0300

    solve errors

--- src/App.jsx
+++ src/App.jsx
@@ -9,9 +9,7 @@ const CourseDetails = lazy(() => import("./pages/CourseDetails"));
 const Courses = lazy(() => import("./pages/Courses"));
 const Layout = lazy(() => import("./pages/Layout"));
 const Login = lazy(() => import("./pages/Login"));
-const Profile = lazy(() => import("./pages/Profile"));
 const Register = lazy(() => import("./pages/Register"));
-const PaymentPage = lazy(() => import("./pages/PaymentPage"));
 const PaymentPagewithId = lazy(() => import("./pages/PaymentPagewithId"));
 import { Box } from "@mui/material";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
@@ -26,10 +24,8 @@ function App() {
         { path: "dashboard", element: <AdminDashboard /> },
         { path: "courses", element: <Courses /> },
         { path: ":id", element: <Courses /> },
-        { path: "profile", element: <Profile /> },
         { path: "register", element: <Register /> },
         { path: "login", element: <Login /> },
-        { path: "enroll", element: <PaymentPage /> },
         { path: "/course/:id", element: <CourseDetails /> },
         { path: "/enroll/:id", element: <PaymentPagewithId /> },
       ],
